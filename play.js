c16872842ec9d2c5fd0d8e2291570092054d8f2a
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-06-02 20:55:43 +0000

    Updated Play.js

--- play.js
+++ play.js
@@ -9,35 +9,23 @@ snakeConn.on("connect", () =>{
   //snakeConn.write("connected");
   console.log("connected to snake server");
   snakeConn.write("Name: BSG");
+  const up = "Move: up";
+  const down = "Move: down";
+  const left = "Move: left";
+  const right = "Move: right";
+
+  const moveUp = setInterval(() => {
+    snakeConn.write(up);
+  }, 50);
+
+  setTimeout(() => {
+    clearInterval(moveUp);
+  }, 300);
+  
 });
 
 snakeConn.on('data', (data) => {
   console.log(data);
 });
 
-/*
-const conn = connect
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () =>{
-    conn.write("connected");
-  });
-
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
-connect();
-
-*/
