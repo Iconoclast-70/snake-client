5e0855c948eae7709d040ceb9616621704cc3129
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-06-03 00:52:54 +0000

    Input moves

--- input.js
+++ input.js
@@ -1,10 +1,43 @@
+let connection;
+
 const handleUserInput = function(key) {
-  if (key === '\u0003') {
+
+  if (key === '\u0077') {
+    const moveUp = setInterval(() => {
+      connection.write("Move: up");
+    }, 50);
+    setTimeout(() => {
+      clearInterval(moveUp);
+    }, 100);
+  } else if (key === '\u0061') {
+    const moveUp = setInterval(() => {
+      connection.write("Move: left");
+    }, 50);
+    setTimeout(() => {
+      clearInterval(moveUp);
+    }, 100);
+  } else if (key === '\u0073') {
+    const moveUp = setInterval(() => {
+      connection.write("Move: down");
+    }, 50);
+    setTimeout(() => {
+      clearInterval(moveUp);
+    }, 100);
+  } else if (key === '\u0064') {
+    const moveUp = setInterval(() => {
+      connection.write("Move: right");
+    }, 50);
+    setTimeout(() => {
+      clearInterval(moveUp);
+    }, 100);
+  } else if (key === '\u0003') {
     process.exit();
   }
+ 
 };
 
-const setupInput = function() {
+const setupInput = function(snakeConn) {
+  connection = snakeConn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
--- play.js
+++ play.js
@@ -6,19 +6,18 @@ const snakeConn = connect("135.23.222.131",50542);
 // interpret incoming data as text
 snakeConn.setEncoding("utf8");
 
-setupInput();
+//setupInput(snakeConn);
 
 snakeConn.on("connect", () =>{
+  
   //snakeConn.write("connected");
   console.log("connected to snake server");
   snakeConn.write("Name: BSG");
-  const up = "Move: up";
-  const down = "Move: down";
-  const left = "Move: left";
-  const right = "Move: right";
+
+  setupInput(snakeConn);
 
   const moveUp = setInterval(() => {
-    snakeConn.write(up);
+    snakeConn.write("Move: up");
   }, 50);
 
   setTimeout(() => {
